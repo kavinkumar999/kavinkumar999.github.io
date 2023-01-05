ca830ccee127df7753ef5a967a0991805cfa006f
kavinkumar999 <kavinkumarnkm007@gmail.com>
2023-01-05 20:08:18 +0530

    feat: Home page -v1

---
+++ static/js/script.js
@@ -0,0 +1,9 @@
+var options = {
+  strings: ['SoftWare Developer', 'Open source enthusiastic'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  smartBackspace: true,
+  loop: true,
+};
+
+var typed = new Typed('.typed-out-word', options);
