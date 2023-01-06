e721f6713a222c6c1070a711bf1fbdd2e003b555
kavinkumar999 <kavinkumarnkm007@gmail.com>
2023-01-06 10:24:49 +0530

    feat: generate public

---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+var options = {
+  strings: ['Love ❤️ to contribute Open Source', 'Passionate to learn New Skills', 'Love to Travel Adventures places'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  smartBackspace: true,
+  loop: true,
+};
+
+var typed = new Typed('.typed-out-word', options);
