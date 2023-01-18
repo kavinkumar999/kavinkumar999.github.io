b935aac365a948e3d6d7f701eb075d45965c0593
kavinkumar999 <kavinkumarnkm007@gmail.com>
2023-01-18 17:43:14 +0530

    feat: add toggle theme feature

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,6 @@
+const DARK_MODE = 'dark-theme';
+const LIGHT_MODE = 'light-theme';
+var url = window.location.host;
 var options = {
   strings: ['Love ❤️ to contribute Open Source', 'Passionate to learn New Skills', 'Love to Travel Adventures places'],
   typeSpeed: 100,
@@ -6,3 +9,17 @@ var options = {
 };
 
 var typed = new Typed('.typed-out-word', options);
+
+function setMode() {
+  isDarkTheme = document.body.classList.contains('dark-theme');
+  let url = window.location.host;
+  if(isDarkTheme) {
+    document.body.classList.add(LIGHT_MODE);
+    document.body.classList.remove(DARK_MODE);
+    document.getElementById("theme").src = `//${url}/png/moon.png`;
+  } else {
+    document.body.classList.add(DARK_MODE);
+    document.body.classList.remove(LIGHT_MODE);
+    document.getElementById("theme").src = `//${url}/png/sun.png`;
+  }
+}
