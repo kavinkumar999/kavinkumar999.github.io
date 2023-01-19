7fb329e70ce1fcd13ab556e0cc5fe0a6827b3b1a
kavinkumar999 <kavinkumarnkm007@gmail.com>
2023-01-19 14:07:37 +0530

    feat: update the theme format

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,4 @@
-const DARK_MODE = 'dark-theme';
 const LIGHT_MODE = 'light-theme';
-var url = window.location.host;
 var options = {
   strings: ['Love ❤️ to contribute Open Source', 'Passionate to learn New Skills', 'Love to Travel Adventures places'],
   typeSpeed: 100,
@@ -11,15 +9,10 @@ var options = {
 var typed = new Typed('.typed-out-word', options);
 
 function setMode() {
-  isDarkTheme = document.body.classList.contains('dark-theme');
-  let url = window.location.host;
-  if(isDarkTheme) {
-    document.body.classList.add(LIGHT_MODE);
-    document.body.classList.remove(DARK_MODE);
-    document.getElementById("theme").src = `//${url}/png/moon.png`;
-  } else {
-    document.body.classList.add(DARK_MODE);
-    document.body.classList.remove(LIGHT_MODE);
-    document.getElementById("theme").src = `//${url}/png/sun.png`;
-  }
+  document.body.classList.toggle(LIGHT_MODE);
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const parentElement = document.getElementById("Home");
+  parentElement.style.height = (window.innerHeight - 87) + "px";
+});
