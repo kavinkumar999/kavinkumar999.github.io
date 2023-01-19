dc39013521738afbbd98f926a845eebfa308d30f
kavinkumar999 <kavinkumarnkm007@gmail.com>
2023-01-19 14:17:59 +0530

    feat: set same theme across the all the pages

--- static/js/script.js
+++ static/js/script.js
@@ -10,6 +10,12 @@ var typed = new Typed('.typed-out-word', options);
 
 function setMode() {
   document.body.classList.toggle(LIGHT_MODE);
+  isLightMode = document.body.classList.contains(LIGHT_MODE);
+  if(isLightMode) {
+    window.localStorage.setItem("theme", "light-theme");
+  } else {
+    window.localStorage.setItem("theme", "dark-theme");
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
