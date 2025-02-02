de124a5916609899e6f24b27fdf8e90c78bfdf7f
Kavin Kumar <kavinkumarnkm007@gmail.com>
2025-02-02 16:41:36 +0530

    feat: implement the cache

--- next.config.mjs
+++ next.config.mjs
@@ -17,10 +17,56 @@ const nextConfig = {
             key: 'Access-Control-Allow-Headers',
             value: 'Content-Type, Authorization',
           },
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        source: '/:path*(jpg|jpeg|png|gif|ico|svg|webp)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        source: '/:path*(ttf|woff|woff2)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        source: '/:path*(css|js)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        source: '/:path*.html',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=3600, must-revalidate',
+          },
         ],
       },
     ];
   },
+  images: {
+    domains: ['kavinkumar.me'],
+    minimumCacheTTL: 60,
+    formats: ['image/webp', 'image/avif'],
+  },
+  compress: true,
 };
 
 export default nextConfig;
